42423e02f222fd0d34b0d0cd3c0366ff7c53f608
umarr <umarr.developer@gmail.com>
2025-09-12 16:50:17 +0600

    set only light theme, delete theme switch cubit

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,6 @@
-import 'package:app/blocs/cubit/theme_cubit.dart';
 import 'package:app/core/router/router.dart';
 import 'package:app/shared/theme/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
@@ -15,31 +13,14 @@ class AppScreen extends StatefulWidget {
 
 class _AppScreenState extends State<AppScreen> {
   final appRoute = AppRouter();
-  final themeCubit = ThemeCubit();
-
-  @override
-  void initState() {
-    themeCubit.init();
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [BlocProvider<ThemeCubit>(create: (context) => themeCubit)],
-      child: BlocBuilder<ThemeCubit, bool?>(
-        bloc: themeCubit,
-        builder: (context, state) {
-          return MaterialApp.router(
-            title: 'Flutter Template',
-            theme: state == true ? darkTheme : lightTheme,
-            routerConfig: appRoute.config(
-              navigatorObservers: () => [
-                TalkerRouteObserver(GetIt.I<Talker>()),
-              ],
-            ),
-          );
-        },
+    return MaterialApp.router(
+      title: 'DaamduuQR',
+      theme: lightTheme,
+      routerConfig: appRoute.config(
+        navigatorObservers: () => [TalkerRouteObserver(GetIt.I<Talker>())],
       ),
     );
   }
