9481ac87cfdae9d897e248960a08c39718b4d837
umarr <umarr.developer@gmail.com>
2025-10-07 19:15:57 +0600

    added new properties for CustomIconButton

--- lib/shared/widgets/icon_button.dart
+++ lib/shared/widgets/icon_button.dart
@@ -7,12 +7,16 @@ class CustomIconButton extends StatefulWidget {
     required this.icon,
     required this.onTap,
     this.size = 24,
+    this.radius = 12,
+    this.animation = true,
     this.background,
     this.foreground,
   });
   final IconData icon;
   final void Function() onTap;
+  final double radius;
   final double size;
+  final bool animation;
   final Color? background;
   final Color? foreground;
 
@@ -21,6 +25,23 @@ class CustomIconButton extends StatefulWidget {
 }
 
 class _CustomIconButtonState extends State<CustomIconButton> {
+  double scale = 1;
+
+  Future onPressed() async {
+    if (widget.animation) {
+      setState(() {
+        scale = 0.8;
+      });
+      await Future.delayed(const Duration(milliseconds: 175));
+      widget.onTap();
+      setState(() {
+        scale = 1;
+      });
+    } else {
+      widget.onTap();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -28,14 +49,21 @@ class _CustomIconButtonState extends State<CustomIconButton> {
       height: widget.size + 16,
       width: widget.size + 16,
       child: IconButton(
-        onPressed: widget.onTap,
+        onPressed: onPressed,
         style: IconButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusGeometry.circular(widget.radius),
+          ),
           backgroundColor: widget.background ?? theme.custom.primaryForeground,
         ),
-        icon: Icon(
-          widget.icon,
-          size: widget.size,
-          color: widget.foreground ?? theme.custom.white,
+        icon: AnimatedScale(
+          scale: scale,
+          duration: const Duration(milliseconds: 175),
+          child: Icon(
+            widget.icon,
+            size: widget.size,
+            color: widget.foreground ?? theme.custom.white,
+          ),
         ),
       ),
     );
