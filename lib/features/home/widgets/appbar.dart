f71369604a3f384547f11dd66c2648dd62c6ff9d
umarr <umarr.developer@gmail.com>
2025-10-14 15:48:33 +0600

    added tilt effect for logo on HomeScreen appbar

--- lib/features/home/widgets/appbar.dart
+++ lib/features/home/widgets/appbar.dart
@@ -1,6 +1,7 @@
 import 'package:app/shared/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:flutter_tilt/flutter_tilt.dart';
 
 class HomeAppBar extends StatelessWidget {
   const HomeAppBar({super.key});
@@ -9,20 +10,7 @@ class HomeAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return SliverAppBar(
-      title: Row(
-        spacing: 8,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(6),
-            decoration: BoxDecoration(
-              color: theme.custom.primaryColor,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: SvgPicture.asset('assets/svg/icon.svg', height: 20),
-          ),
-          SvgPicture.asset('assets/svg/logo.svg', height: 20),
-        ],
-      ),
+      title: _AppBarTitle(),
       backgroundColor: theme.custom.primaryBackground,
       pinned: true,
       actions: [
@@ -39,6 +27,35 @@ class HomeAppBar extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class _AppBarTitle extends StatelessWidget {
+  const _AppBarTitle();
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Tilt(
+      lightShadowMode: LightShadowMode.base,
+      shadowConfig: ShadowConfig(disable: true),
+      tiltConfig: TiltConfig(angle: 8),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        spacing: 8,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(6),
+            decoration: BoxDecoration(
+              color: theme.custom.primaryColor,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: SvgPicture.asset('assets/svg/icon.svg', height: 20),
+          ),
+          SvgPicture.asset('assets/svg/logo.svg', height: 20),
+        ],
+      ),
     );
   }
 }
