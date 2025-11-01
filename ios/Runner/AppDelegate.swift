330b8349e984cb2dbedbf8dd8ab9b3b60b3d973f
umarr <umarr.developer@gmail.com>
2025-11-01 18:37:03 +0600

    fixed invalid AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,16 +1,13 @@
-import UIKit
 import Flutter
-import FirebaseCore
-import FirebaseMessaging
+import UIKit
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
-    UNUserNotificationCenter.current().delegate = self
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
