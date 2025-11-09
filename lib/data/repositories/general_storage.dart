88c4f09fd17929451c6ac4a016379617691b6130
umarr <umarr.developer@gmail.com>
2025-11-09 15:55:36 +0600

    update GeneralStorage methods

--- lib/data/repositories/general_storage.dart
+++ lib/data/repositories/general_storage.dart
@@ -4,7 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:hive_ce_flutter/adapters.dart';
 import 'package:path_provider/path_provider.dart';
 
-enum GeneralStorageKey { isDarkMode }
+enum GeneralStorageKey { favorites }
 
 class GeneralStorage {
   late final Box box;
@@ -18,19 +18,23 @@ class GeneralStorage {
     box = await Hive.openBox('general_storage');
   }
 
-  Future setValue(GeneralStorageKey key, dynamic value) async {
+  Future write(GeneralStorageKey key, dynamic value) async {
     await box.put(key.name, value);
   }
 
-  dynamic getValue(GeneralStorageKey key) {
+  dynamic read(GeneralStorageKey key) {
     return box.get(key.name);
   }
 
-  Future write(String key, value) async {
+  Future delete(GeneralStorageKey key) async {
+    await box.delete(key);
+  }
+
+  Future writeObject(String key, value) async {
     await box.put(key, value.toJson());
   }
 
-  Map<String, dynamic>? read(String key) {
+  Map<String, dynamic>? readObject(String key) {
     return box.get(key) as Map<String, dynamic>?;
   }
 }
